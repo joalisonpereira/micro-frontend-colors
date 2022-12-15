3bc73c4b3ccc9f286de63f53b58c18d21ae28083
joalisonpereira <joalison.pereira@hotmail.com>
2022-12-15 02:17:36 -0300

    implementing gh build

--- root-config/src/colors-root-config.ts
+++ root-config/src/colors-root-config.ts
@@ -1,33 +1,13 @@
 import { registerApplication, start, LifeCycles } from "single-spa";
+import config from "./config.json";
 
-registerApplication({
-  name: "@colors/navbar",
-  app: () => System.import<LifeCycles>("@colors/navbar"),
-  activeWhen: () => true,
-});
-
-registerApplication({
-  name: "@colors/boxes",
-  app: () => System.import<LifeCycles>("@colors/boxes"),
-  activeWhen: (location) => location.pathname === "/",
-});
-
-registerApplication({
-  name: "@colors/blue",
-  app: () => System.import<LifeCycles>("@colors/blue"),
-  activeWhen: (location) => location.pathname === "/blue",
-});
-
-registerApplication({
-  name: "@colors/gold",
-  app: () => System.import<LifeCycles>("@colors/gold"),
-  activeWhen: ["/gold"],
-});
-
-registerApplication({
-  name: "@single-spa/welcome",
-  app: () => System.import<LifeCycles>("@single-spa/welcome"),
-  activeWhen: (location) => location.pathname === "/welcome",
+config.applications.forEach((item) => {
+  registerApplication({
+    name: item.name,
+    app: () => System.import<LifeCycles>(item.package),
+    activeWhen: (location) =>
+      item.activeWhen === true ? true : location.pathname === item.activeWhen,
+  });
 });
 
 start({ urlRerouteOnly: true });
